81c24f871622ae5d7d6ceffb8bb5728bc3f56512
nexora-droid <ashlesh.deshmukh.sg@gmail.com>
2026-06-04 22:47:04 +0800

    pagination

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.title("Morse Code Trainer")
+home_page = st.Page("home.py", title="Home", icon=":material/home:")
+faq_page = st.Page("faq.py", title="FAQ", icon=":material/help:")
+pg = st.navigation([home_page, faq_page])
+st.set_page_config(initial_sidebar_state="collapsed")
+pg.run()
