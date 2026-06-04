49b946529bbc258ee261c53c595f4fdb885cc964
nexora-droid <ashlesh.deshmukh.sg@gmail.com>
2026-06-04 22:50:33 +0800

    switching to flask. not gonna change now.

--- main.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-home_page = st.Page("home.py", title="Home", icon=":material/home:")
-faq_page = st.Page("faq.py", title="FAQ", icon=":material/help:")
-pg = st.navigation([home_page, faq_page])
-st.set_page_config(initial_sidebar_state="collapsed")
-pg.run()
